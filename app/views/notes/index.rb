e37ac39bd2a6460c11000aa5df273dcaa0039d53
Mark Delk <24943868+mddelk@users.noreply.github.com>
2026-07-05 19:11:11 -0500

    stub out notes.index list

--- app/views/notes/index.rb
+++ app/views/notes/index.rb
@@ -1,7 +1,20 @@
+require "faker"
+
 module Notes
   module Views
     module Notes
       class Index < ::Notes::View
+        Note = Data.define(:id, :title, :content)
+
+        expose :notes do
+          Array.new(10) do
+            Note.new(
+              id: SecureRandom.uuid,
+              title: Faker::ProgrammingLanguage.name,
+              content: Faker::Lorem.paragraph
+            )
+          end
+        end
       end
     end
   end
--- test/features/notes/index_test.rb
+++ test/features/notes/index_test.rb
@@ -4,6 +4,7 @@ class Notes::IndexTest < Hanami::Minitest::FeatureTest
   test "should list notes" do
     visit "/notes"
 
-    assert_element "h1", text: "Notes::Views::Notes::Index"
+    assert_element "h1", text: "Notes"
+    assert_selector "#notes > li > article > h2", count: 10
   end
 end
