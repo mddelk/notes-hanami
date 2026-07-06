40912446ecfc514f35f8ddaf2cd38a6b14e039bf
Mark Delk <24943868+mddelk@users.noreply.github.com>
2026-07-06 00:53:59 -0500

    auth: add session support

--- config/app.rb
+++ config/app.rb
@@ -3,5 +3,11 @@ require "hanami"
 module Notes
   class App < Hanami::App
     config.views.default_template_engine = "slim"
+
+    config.actions.sessions = :cookie, {
+      key: "notes.session",
+      secret: settings.session_secret,
+      expire_after: 60 * 60 * 24 * 365
+    }
   end
 end
--- config/settings.rb
+++ config/settings.rb
@@ -1,7 +1,5 @@
 module Notes
   class Settings < Hanami::Settings
-    # Define your app settings here, for example:
-    #
-    # setting :my_flag, default: false, constructor: Types::Params::Bool
+    setting :session_secret, constructor: Types::String
   end
 end
